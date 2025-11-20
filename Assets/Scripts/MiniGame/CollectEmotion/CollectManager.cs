07d460f464fa498b3c65dd2982d1a3d3bbf4bf1d
GAUDUL <limjsu12@naver.com>
2025-11-21 04:30:45 +0900

    feat: Add Life Image in CollectGame

--- Assets/Scripts/MiniGame/CollectEmotion/CollectManager.cs
+++ Assets/Scripts/MiniGame/CollectEmotion/CollectManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CollectManager : MonoBehaviour
@@ -13,6 +14,9 @@ public class CollectManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI rewardPointsText;
     [SerializeField] private ObjectSpawner spawner;
     [SerializeField] private PlayerTouchController player;
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioClip sfxClip;
+    [SerializeField] private List<GameObject> lifeImage;
 
     private int score = 0;
     private int life = 3;
@@ -25,24 +29,28 @@ public class CollectManager : MonoBehaviour
     public void AddScore()
     {
         score += 10;
+        sfxSource.PlayOneShot(sfxClip);
         UpdateUI();
     }
 
     public void ReduceLife()
     {
         life -= 1;
+        UpdateUI();
+        for(int i = 0; i< lifeImage.Count; i++)
+        {
+            lifeImage[i].SetActive(i < life);
+        }
         if (life <= 0)
         {
             life = 0;
             EndGame();
         }
-        UpdateUI();
     }
 
     void UpdateUI()
     {
         scoreText.text = $"점수 {score}";
-        lifeText.text = $"남은 기회 {life}";
     }
 
     void EndGame()
